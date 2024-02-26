2629a62f9c4e2857937801e2ebea1a73e595bc40
Jeremy Swafford <jeremy@swafford.co>
2024-02-26 00:39:22 -0500

    add ballExplode and more structure

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ canvas.height = 600
 
 canvas.style.border = '1px solid black'
 
+let frame = 0
+
+const animate = () => {
+  frame++
+  experiments[currentExperiment].fn()
+  requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate)
+
 const ball = {
   x: canvas.width / 2 - 5,
   y: canvas.height / 2 - 5,
@@ -13,23 +23,25 @@ const ball = {
   speed: 4,
   dx: 4,
   dy: -4,
+  visible: true,
+  strokeStyle: 'black',
+  lineWidth: 4,
+  fillStyle: 'red',
 }
-
-context.beginPath()
-context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
-context.fillStyle = 'red'
-context.fill()
-
-let currentExperiment = 0
-
-const animate = () => {
-  experiments[currentExperiment].fn()
-  requestAnimationFrame(animate)
-}
-
-requestAnimationFrame(animate)
 
 const bounceBallLikeDVDScreensaver = () => {
+  if (lastExperiment !== 'Ball Bounce DVD') {
+    lastExperiment = 'Ball Bounce DVD'
+    console.log('Ball Bounce DVD')
+    ball.visible = true
+    ball.lineWidth = 4
+    context.lineWidth = 4
+    context.stroke()
+    // ball.x = canvas.width / 2 - 5
+    // ball.y = canvas.height / 2 - 5
+    // ball.dx = 4
+    // ball.dy = -4
+  }
   // toggle the following line to see the ball color in the canvas
   context.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -37,6 +49,8 @@ const bounceBallLikeDVDScreensaver = () => {
   context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
   context.fillStyle = 'red'
   context.fill()
+  context.strokeStyle = 'black'
+  context.stroke()
 
   ball.x += ball.dx
   ball.y += ball.dy
@@ -53,16 +67,50 @@ const bounceBallLikeDVDScreensaver = () => {
   document.getElementById('stat2').innerHTML = `DX: ${ball.dx}, DY: ${ball.dy}`
 }
 
+
+const ballExplode = () => {
+  if (lastExperiment !== 'Ball Explode') {
+    console.log('Ball Explode')
+    lastExperiment = 'Ball Explode'
+    ball.lineWidth = 4
+    ball.visible = true
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  if (ball.lineWidth < 0) {
+    ball.visible = false
+    return
+  }
+
+  context.beginPath()
+  context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
+  context.strokeStyle = ball.strokeStyle
+  context.lineWidth = ball.lineWidth
+  context.stroke()
+  console.log('ball.lineWidth', ball.lineWidth)
+
+  ball.lineWidth -= 0.2
+  ball.radius += 3
+}
+
+let currentExperiment = 0
+let lastExperiment = ''
+
+const experiments = [{
+  name: 'Ball Bounce DVD',
+  fn: bounceBallLikeDVDScreensaver,
+}, {
+  name: 'Ball Explode',
+  fn: ballExplode,
+}]
+
 const startExperiment = (experiment) => {
   currentExperiment = experiment
   requestAnimationFrame(experiments[experiment].fn)
 }
 
-const experiments = [{
-  name: 'Ball Bounce DVD',
-  fn: bounceBallLikeDVDScreensaver,
-}]
-
-document.getElementById('stat3').innerHTML = experiments.map((e, i) => {
+const experimentButtons = document.getElementById('stat3')
+experimentButtons.classList.add('experiment-buttons')
+experimentButtons.innerHTML = experiments.map((e, i) => {
   return `<button onclick="startExperiment(${i})">${e.name}</button>`
 }).join('')
