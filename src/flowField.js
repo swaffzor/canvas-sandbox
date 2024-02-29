a14214c2aa84e661c6e016c9df053be736869f09
Jeremy Swafford <jeremy@swafford.co>
2024-02-28 23:13:40 -0500

    pattern explorer

--- src/flowField.js
+++ src/flowField.js
@@ -2,6 +2,7 @@ let canvas;
 let ctx;
 let flowField;
 let flowFieldAnimation
+let isPaused = false
 
 window.onload = () => {
   canvas = document.getElementById('myCanvas')
@@ -20,6 +21,16 @@ window.addEventListener('resize', () => {
   flowField.animate(0)
 })
 
+window.addEventListener('click', () => {
+  if (isPaused) {
+    flowField.animate(0)
+    isPaused = false
+  } else {
+    cancelAnimationFrame(flowFieldAnimation)
+    isPaused = true
+  }
+})
+
 const mouse = {
   x: 0,
   y: 0
@@ -35,7 +46,7 @@ class FlowField {
   #height
   constructor(ctx, width, height) {
     this.#ctx = ctx
-    this.#ctx.strokeStyle = 'white'
+    this.#ctx.lineWidth = 1
     this.#width = width
     this.#height = height
     this.angle = 0
@@ -43,12 +54,34 @@ class FlowField {
     this.interval = 1000 / 60 // 60 fps
     this.timer = 0
     this.cellSize = 15
+    this.gradient
+    this.#createGradient()
+    this.#ctx.strokeStyle = this.gradient
+    this.radius = .5
+    this.vr = 0.03 // veloicty of radius
   }
-  #drawLine(x, y) {
-    const length = 300
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height)
+    this.gradient.addColorStop(0.1, '#ff5c33')
+    this.gradient.addColorStop(0.2, '#ff66b3')
+    this.gradient.addColorStop(0.4, '#ccccff')
+    this.gradient.addColorStop(0.6, '#b3ffff')
+    this.gradient.addColorStop(0.8, '#80ff80')
+    this.gradient.addColorStop(0.9, '#ffff33')
+  }
+  #drawLine(angle, x, y) {
+    let positionX = x
+    let positionY = y
+    let dx = mouse.x - positionX
+    let dy = mouse.y - positionY
+    let distance = (dx * dx + dy * dy) // Math.sqrt is slow
+    if (distance > 600000) distance = 600000
+    else if (distance < 10000) distance = 10000
+
+    let length = distance * 0.001
     this.#ctx.beginPath()
     this.#ctx.moveTo(x, y)
-    this.#ctx.lineTo(mouse.x, mouse.y)
+    this.#ctx.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length)
     this.#ctx.stroke()
   }
   animate(timeStamp) {
@@ -56,11 +89,13 @@ class FlowField {
     this.lastTime = timeStamp
     if (this.timer > this.interval) {
       this.#ctx.clearRect(0, 0, this.#width, this.#height)
+      // this.radius += this.vr
+      // if (this.radius > 5 || this.radius < -5) this.vr *= -1
 
       for (let y = 0; y < this.#height; y += this.cellSize) {
         for (let x = 0; x < this.#width; x += this.cellSize) {
-          this.#drawLine(x, y)
-
+          const angle = (Math.cos(mouse.x * x * 0.0001) + Math.sin(mouse.y * y * 0.0001)) * this.radius
+          this.#drawLine(angle, x, y)
         }
       }
 
